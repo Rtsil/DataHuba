2157f436f03ca8c8e6afd1868d63a2b25cde3fcc
tsilavo <rtsilavotahina@gmail.com>
2023-07-22 08:38:47 +0300

    "Getting data from api to database and show in search_result.html"

--- country/urls.py
+++ country/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/', views.search_result, name='search')
+    path('search/', views.search_result, name='search'),
+    path('<str:country_name>', views.country, name='country')
 ]
--- country/views.py
+++ country/views.py
@@ -8,15 +8,18 @@ rest_county_url = 'https://restcountries.com/v3.1'
 # Create your views here.
 
 def index(request):
-    return render(request, 'country/index.html', {})
+    q = Country.objects.all()
+    q = q.order_by('name')
+    return render(request, 'country/index.html', {'data': q})
 
 
 def search_result(request):
     search_query = request.GET.get('q', '')
-    q = Country.objects.get(name__icontains=search_query)
-    print(q.flag_emoji)
-    if q:
-        return render(request, 'country/search_result.html', {'data': q})
-    else:
-        data = {'status': 'Not Found'}
-        return render(request, 'country/search_result.html', data)
+    q = Country.objects.filter(name__icontains=search_query)
+    return render(request, 'country/search_result.html', {'data': q})
+
+
+def country(request, country_name):
+    q = Country.objects.get(name__icontains=country_name)
+    return render(request, 'country/country.html', {'data': q})
+
